33fdab48ac0d01fa1c7afb220629694fbc8b5996
chungsuichu-dev <chungsuichu-dev@users.noreply.github.com>
2026-03-24 01:57:30 +0000

    deploy: dde7eda38d1588821176e1f7c41612d2f45e62fb

---
+++ _app/immutable/nodes/5.CMrp96Xw.js
@@ -0,0 +1,7 @@
+import{a as t,f as i}from"../chunks/CttPK3hj.js";import"../chunks/F2D2ZbGV.js";var s=i(`<div class="page svelte-fo0yqp"><div class="content svelte-fo0yqp"><div class="title svelte-fo0yqp">[Mission]</div> While organizing old family photos, I found faces I didn’t recognize.
+
+        To me, they were just images — but to someone else, they might be priceless memories.
+
+        Wouldn’t it be wonderful if people could see what they love?
+
+        🌸With your help, Old Photos Connect can make it happen.🌸</div></div>`);function n(e){var o=s();t(e,o)}export{n as component};
---
+++ _app/immutable/nodes/6.DamtWYIk.js
@@ -0,0 +1,15 @@
+import{a,f as s}from"../chunks/CttPK3hj.js";import"../chunks/F2D2ZbGV.js";var t=s(`<div class="page svelte-17wjoog"><div class="content svelte-17wjoog"><div class="title svelte-17wjoog">[Rules]</div> 1) Users stay anonymous.
+
+2) Only upload photos you have the right to share.
+
+3) Share only respectful photos.
+
+4) Avoid personal details for living individuals. General clues ok.
+
+5) We do not verify the accuracy of captions.
+
+6) You can delete any inappropriate photo.
+
+7) No AI images please.
+
+8) If a photo could be meaningful for someone, it belongs here.🌹</div></div>`);function n(o){var e=t();a(o,e)}export{n as component};
